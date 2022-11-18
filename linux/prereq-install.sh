9cb80e42090cf0d4b6ea8b99e788b61c3e1d1865
Ubuntu <vmuser@vm01.q4yqgxfsilcu1i1kq1y4pdlmyc.cx.internal.cloudapp.net>
2022-11-18 16:56:28 +0000

    1. Archivo de prerequisitos creado.

---
+++ linux/prereq-install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "Actualizacion de Repos"
+sudo apt update
+echo "Instalacion de Azure CLI"
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+echo "Instalacion de Docker"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+#echo "-----------------------------"
+#echo "    Docker Status            "
+#echo "-----------------------------"
+#sudo systemctl status docker
+echo "Installacion de Kubernetes"
+sudo apt -y install curl apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt -y install vim git curl wget kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+kubectl version --client && kubeadm version
+echo "Instalacion Helm"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+
